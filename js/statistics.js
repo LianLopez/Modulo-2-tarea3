3757b9725b621dcad4c23a1a48e3019481febed4
LianLopez <lopez.leonardo.info@gmail.com>
2019-05-08 12:14:47 -0300

    files renamed && starting task 3

---
+++ js/statistics.js
@@ -0,0 +1,15 @@
+var statistics = {
+    "number-of-democrats": 0,
+    "number-of-republicans": 0,
+    "number-of-independents": 0,
+    "total": 0,
+    "democrats-average-votes-with-party": 0,
+    "republicans-average-votes-with-party": 0,
+    "independents-average-votes-with-party": 0,
+    "total-average": 0,
+    "least-engaged": [],
+    "most-engaged": [],
+    "least-loyal": [],
+    "most-loyal": []
+}
+    
