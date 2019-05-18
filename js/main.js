ffb774905792177f37ec3dc8b085a74cf903930a
LianLopez <lopez.leonardo.info@gmail.com>
2019-05-18 16:16:59 -0300

    minor fixs

--- js/main.js
+++ js/main.js
@@ -23,3 +23,47 @@ function obtenerValores() {
 }
 
 obtenerValores()
+
+
+
+function generarTabla(key1, key2, page) {
+	var htmlTablaCantidad = ""
+	htmlTablaCantidad += "<tr>"
+	htmlTablaCantidad += "<td>Republicans</td>"
+	htmlTablaCantidad += "<td>" + statistics["number-of-republicans"] + "</td>"
+	htmlTablaCantidad += "<td>" + statistics["republicans-average-votes-with-party"] + "</td>"
+	htmlTablaCantidad += "</tr>"
+	htmlTablaCantidad += "<tr>"
+	htmlTablaCantidad += "<td>Democrats</td>"
+	htmlTablaCantidad += "<td>" + statistics["number-of-democrats"] + "</td>"
+	htmlTablaCantidad += "<td>" + statistics["democrats-average-votes-with-party"] + "</td>"
+	htmlTablaCantidad += "</tr>"
+	htmlTablaCantidad += "<tr>"
+	htmlTablaCantidad += "<td>Independents</td>"
+	htmlTablaCantidad += "<td>" + statistics["number-of-independents"] + "</td>"
+	htmlTablaCantidad += "<td>" + statistics["independents-average-votes-with-party"] + "</td>"
+	htmlTablaCantidad += "</tr>"
+	htmlTablaCantidad += "<td>Total</td>"
+	htmlTablaCantidad += "<td>" + statistics["total"] + "</td>"
+	htmlTablaCantidad += "<td>" + statistics["total-average"] + "</td>"
+	htmlTablaCantidad += "</tr>"
+
+	document.getElementById("tabla-cantidad").innerHTML = htmlTablaCantidad
+
+	document.getElementById("tabla2").innerHTML = statistics[key1].map(element => {
+		var tabla1 = ""
+		tabla1 += "<tr><td>" + element.first_name
+		element.middle_name == null ? tabla1 += " " : tabla1 += " " + element.middle_name + " "
+		tabla1 += element.last_name + "</a></td>"
+		page? tabla1 += "<td>" + element.missed_votes + "</td><td>" + element.missed_votes_pct + "</td>" : tabla1 += "<td>" + (element.total_votes - element.missed_votes) + "</td><td>" + element.votes_with_party_pct + "</td>"
+		return tabla1
+	}).join("")
+	document.getElementById("tabla1").innerHTML = statistics[key2].map(element => {
+		var tabla2 = ""
+		tabla2 += "<tr><td>" + element.first_name
+		element.middle_name == null ? tabla2 += " " : tabla2 += " " + element.middle_name + " "
+		tabla2 += element.last_name + "</a></td>"
+		page? tabla2 += "<td>" + element.missed_votes + "</td><td>" + element.missed_votes_pct + "</td>" : tabla2 += "<td>" + (element.total_votes - element.missed_votes) + "</td><td>" + element.votes_with_party_pct + "</td>"
+		return tabla2
+	}).join("")
+}
